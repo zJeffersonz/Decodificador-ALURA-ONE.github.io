daf15dff692038bf8b1c8982499a93dee902a4ee
Jefferson <104679945+zJeffersonz@users.noreply.github.com>
2022-12-25 13:59:18 -0300

    Add files via upload

---
+++ arquivo.js
@@ -0,0 +1,29 @@
+var textoDigitado = document.querySelector("#area-texto"); 
+var textoGravado = document.querySelector("#texto-saida");
+
+function criptografar(){
+    var texto = textoDigitado.value; 
+
+    var resultadoCrip = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai")
+    .replace(/o/g, "ober").replace(/u/g, "ufat");
+    
+    document.getElementById("texto-saida").innerHTML ='<textarea  id="resultado" >'+ resultadoCrip +'</textarea>' +
+     '<button id="bt-copiar" onclick="copiar()">Copiar</button>';
+   
+};
+function descriptografar(){
+    var texto = textoDigitado.value;
+
+    var resultadoDesc = texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/UFAT/g, "U");
+
+    document.getElementById("texto-saida").innerHTML = '<textarea  id="resultado" >'+ resultadoDesc +'</textarea>' +
+     '<button id="bt-copiar" onclick="copiar()">Copiar</button>';
+};
+
+function copiar() {
+    var textoCopiado = document.getElementById('resultado');
+  
+    textoCopiado.select();
+    document.execCommand('copy');
+    alert("Texto copiado.");
+} 
